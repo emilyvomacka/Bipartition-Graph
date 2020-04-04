c903a5806dfa3991c28fef52fdf503156e7bf6d9
Emily Vomacka <emilyvomacka@gmail.com>
2020-04-04 14:04:42 -0700

    dogs sorted

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,25 @@
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  return true if dislikes.empty?
+  groups = Array.new(dislikes.length)
+  groups.each_index do |n|
+    if groups[n].nil? 
+      groups[n] = 0
+      queue = []
+      queue.push(n)
+      while queue.length > 0 
+        curr_index = queue.shift
+        dogs_we_hate = dislikes[curr_index]
+        dogs_we_hate.each do |dog|
+          if groups[dog].nil?
+            groups[dog] = 1 - groups[curr_index]
+            queue.push(dog)
+          else
+            return false if groups[dog] != 1 - groups[curr_index]
+          end 
+        end
+      end 
+    end 
+  end 
+  return !groups.any? { |n| n.nil? }
 end
